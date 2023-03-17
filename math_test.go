a0af439f3399d7ef30dbcb6a57f0b23d30d8618b
Sávio Barbosa <saviiobarbosa@gmail.com>
2023-03-17 09:22:19 -0300

    fix: Corrigindo erros de package

--- math_test.go
+++ math_test.go
@@ -1,9 +1,11 @@
 package main
 
+import "testing"
+
 func TestSoma(t *testing.T) {
-	total := Sum(15, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
-		t.Error("Resultado da soma é invalido: Resultado %d. Esperado %d", total, 30)
+		t.Errorf("Resultado da soma é invalido: Resultado %d. Esperado %d", total, 30)
 	}
 }
